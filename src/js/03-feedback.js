27e4d750ac4e41e6078f3ba7a0d8d31e069dfd5a
Paulo <pau.solanoy@gmail.com>
2023-11-11 15:47:31 +0800

    Update Task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,35 +1,42 @@
-import throttle from "lodash.throttle";
+import throttle from 'lodash.throttle';
 
-const form = document.querySelector("feedback-form");
+const formInfo = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form textarea'),
+  email: document.querySelector('.feedback-form input[type=email]'),
+};
+const input = formInfo.form.elements;
+const storageKey = 'feedback-form-state';
+const savedData = localStorage.getItem(storageKey);
+const formData = {};
 
-const emailInput = document.querySelector("email");
-const messageInput = document.querySelector("message");
+formInfo.form.addEventListener('submit', onFormSubmit);
+formInfo.email.addEventListener('input', throttle(onInput, 500));
+formInfo.textarea.addEventListener('input', throttle(onInput, 500));
 
-const throttledUpdateStorage = throttle(() => {
-  const formData = {
-    email: emailInput.value,
-    message: messageInput.value,
-  };
-  localStorage.setItem("feedback-form-state", JSON.stringify(formData));
-}, 500);
-
-form.addEventListener("input", throttledUpdateStorage);
-
-const formData = JSON.parse(localStorage.getItem("feedback-form-state"));
-
-if (formData) {
-  emailInput.value = formData.email;
-  messageInput.value = formData.message;
+function inputValue() {
+  formData.email = input.email.value;
+  formData.message = input.message.value;
 }
 
-form.addEventListener("submit", (event) => {
+if (savedData) {
+  const formData = JSON.parse(savedData);
+  formInfo.email.value = formData.email;
+  formInfo.textarea.value = formData.message;
+}
+
+function onInput(event) {
+  inputValue();
+  localStorage.setItem(storageKey, JSON.stringify(formData));
+}
+function onFormSubmit(event) {
   event.preventDefault();
-
-  localStorage.removeItem("feedback-form-state");
-  emailInput.value = "";
-  messageInput.value = "";
-
-  const formData = new FormData(form);
-
-  console.log(formData);
-});
+  inputValue();
+  if (input.email.value === '' || input.message.value === '') {
+    alert();
+  } else {
+    console.log(formData);
+    event.currentTarget.reset();
+    localStorage.removeItem(storageKey);
+  }
+}
